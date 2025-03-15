860f95d4ae1b7b3bbf3d00c666886c4cbccea1aa
Hamilton <>
2025-03-15 15:30:23 +0100

    Add Title & Cover components

---
+++ src/page/Cover.tsx
@@ -0,0 +1,33 @@
+import { useRef, ChangeEventHandler } from 'react'
+
+export default function Cover() {
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const onChangeCoverImage = () => {
+    fileInputRef.current?.click()
+  }
+
+  const onCoverImageUpload: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const target = event.target
+    console.log(target?.files?.[0])
+  }
+
+  return (
+    <div className="relative flex justify-center items-center h-80 border-[#eaeaea] border-b-2">
+      <img src="./notion-clone-cover.png" alt="cover"/>
+      <button
+        type="button"
+        className="absolute bottom-5 right-24 p-1.5 opacity-0 bg-white border-2 rounded text-[#6f6F6F] font-bold cursor-pointer"
+        onClick={onChangeCoverImage}
+      >
+        Change cover
+      </button>
+      <input
+        onChange={onCoverImageUpload}
+        className="hidden"
+        ref={fileInputRef}
+        type="file"
+      />
+    </div>
+  )
+}
---
+++ src/page/Title.tsx
@@ -0,0 +1,3 @@
+export default function Title() {
+  return <h1 className="underline text-3xl">Testing</h1>
+}
