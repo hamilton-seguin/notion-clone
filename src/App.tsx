709e459c86d798c37bac9ab8cf8f716cbee27068
Hamilton <>
2025-03-15 13:02:50 +0100

    init Notion Clone w/ Typescript, Supabase, Netlify

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Title from './page/Title'
+
+function App() {
+  return (
+    <main className="flex h-dvh justify-center items-center">
+      <Title />
+    </main>
+  )
+}
+
+export default App
