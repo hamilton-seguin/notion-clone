9361fbc38c48f4a567d743c06be2932893a43e4b
Hamilton <>
2025-03-16 16:45:43 +0100

    Update ThemeToggle text

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -16,24 +16,24 @@ export default function ThemeToggle() {
     document.body.classList.toggle('dark')
   }
   return (
-    <label className="inline-flex items-center cursor-pointer relative">
-      <input
-        type="checkbox"
-        onChange={handleToggle}
-        checked={isDark}
-        className="sr-only peer"
-      />
-      <div className="relative w-14 h-8 bg-gray-200 rounded-full peer dark:bg-zinc-600"></div>
-      <div className="absolute top-1 left-1 h-6 w-6 rounded-full bg-white flex items-center justify-center transform transition-transform duration-300 peer-checked:translate-x-6">
-        {isDark ? (
-          <img src="./moon.svg" alt="Dark mode" className="h-4.5 w-4.5" />
-        ) : (
-          <img src="./sun.svg" alt="Light mode" className="h-4.5 w-4.5" />
-        )}
-      </div>
-      <span className="ml-3 text-sm font-medium">
-        Toggle
-      </span>
-    </label>
+    <div className='flex justify-end items-center'>
+      <label className="inline-flex items-center cursor-pointer relative">
+        <input
+          type="checkbox"
+          onChange={handleToggle}
+          checked={isDark}
+          className="sr-only peer"
+        />
+        <div className="w-14 h-8 bg-gray-200 rounded-full peer dark:bg-zinc-600"></div>
+        <div className="absolute top-1 left-1 h-6 w-6 rounded-full bg-white flex items-center justify-center transform transition-transform duration-300 peer-checked:translate-x-6">
+          {isDark ? (
+            <img src="./moon.svg" alt="Dark mode" className="h-4.5 w-4.5" />
+          ) : (
+            <img src="./sun.svg" alt="Light mode" className="h-4.5 w-4.5" />
+          )}
+        </div>
+        <span className="ml-3 text-sm font-medium">{isDark ? 'dark mode' : 'light mode'}</span>
+      </label>
+    </div>
   )
 }
