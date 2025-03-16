dcabc8e530f077b6b329ba98c3442c952bab1ef6
Hamilton <>
2025-03-16 22:31:26 +0100

    Add Page to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Title from '@/page/Title'
-import Cover from '@/page/Cover'
+import { Page } from '@/page'
 import ThemeToggle from '@/components/ThemeToggle'
 
 function App() {
   return (
-    <main className="flex flex-col p-4 h-dvh  bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text">
+    <main className="flex flex-col p-4 h-dvh bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text">
       <ThemeToggle />
       <div className="flex flex-col gap-4 justify-start items-center">
-        <Cover />
-        <Title />
+       <Page />
       </div>
     </main>
   )
