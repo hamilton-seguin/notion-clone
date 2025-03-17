1dc2855cff75da75f28bdd7ba39c2361c8da5593
Hamilton <>
2025-03-17 18:00:19 +0100

    Create useOverflowsScreenBottom to mode CommandPanel above node when not enough space below

---
+++ src/hooks/useOverflowsScreenBottom.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useOverflowsScreenBottom = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [overflows, setOverflows] = useState(false)
+
+  useEffect(() => {
+    if (ref.current) {
+      const { bottom } = ref.current.getBoundingClientRect()
+      const { innerHeight } = window
+      setOverflows(bottom > innerHeight)
+    }
+  }, [])
+
+  return { ref, overflows }
+}
