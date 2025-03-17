30196482570833cc440e6e74911cbc030977c8ff
Hamilton <>
2025-03-17 11:57:50 +0100

    Replace methods in BasicNode with useAppState

--- src/page/Page.tsx
+++ src/page/Page.tsx
@@ -1,40 +1,17 @@
-import { useState } from 'react'
-
-import { NodeData } from '@/types'
-import { useFocusedNodeIndex } from '@/hooks'
-import { Cover, Spacer, Title, BasicNode } from '@/page'
 import { nanoid } from 'nanoid'
 
+import { Cover, Spacer, Title, BasicNode } from '@/page'
+
+import { useFocusedNodeIndex, useAppState } from '@/hooks'
+
 export const Page = () => {
-  const [nodes, setNodes] = useState<NodeData[]>([])
-  const [title, setTitle] = useState('Title')
+  const { title, nodes, setTitle, addNode } = useAppState()
   const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({ nodes })
-
-  const addNode = (node: NodeData, index: number) => {
-    const newNodes = [...nodes]
-    // insert new node at index
-    newNodes.splice(index, 0, node)
-    setNodes(newNodes)
-  }
-
-  const removeNodeByIndex = (index: number) => {
-    const newNodes = [...nodes]
-    // remove 1 node at index
-    newNodes.splice(index, 1)
-    setNodes(newNodes)
-  }
-
-  const changeNodeValue = (index: number, value: string) => {
-    const newNodes = [...nodes]
-    // update value of node at index
-    newNodes[index].value = value
-    setNodes(newNodes)
-  }
 
   return (
     <>
       <Cover />
-      <div className='flex flex-col items-center justify-start w-full'>
+      <div className="flex flex-col items-center justify-start w-full">
         <Title title={title} changePageTitle={setTitle} addNode={addNode} />
         {nodes.map((node, index) => (
           <BasicNode
@@ -43,9 +20,6 @@ export const Page = () => {
             updateFocusedIndex={setFocusedNodeIndex}
             isFocused={focusedNodeIndex === index}
             index={index}
-            addNode={addNode}
-            removeNodeByIndex={removeNodeByIndex}
-            changeNodeValue={changeNodeValue}
           />
         ))}
         <Spacer
