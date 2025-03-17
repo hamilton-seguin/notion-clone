2dd6f1215cdf22d4a050229b367c3aeb600adfa5
Hamilton <>
2025-03-17 11:51:49 +0100

    Create usePageState hook to centralise all different node methods + states

---
+++ src/hooks/usePageState.ts
@@ -0,0 +1,50 @@
+import { NodeData, NodeType, Page } from '@/types'
+import { useImmer } from 'use-immer'
+
+export const usePageState = (initialState: Page) => {
+  const [page, setPage] = useImmer<Page>(initialState)
+
+  const addNode = (node: NodeData, index: number) => {
+    setPage((draft) => draft.nodes.splice(index, 0, node))
+  }
+
+  const removeNodeByIndex = (nodeIndex: number) => {
+    setPage((draft) => draft.nodes.splice(nodeIndex, 1))
+  }
+
+  const changeNodeValue = (nodeIndex: number, value: string) => {
+    setPage((draft) => (draft.nodes[nodeIndex].value = value))
+  }
+
+  const changeNodeType = (nodeIndex: number, type: NodeType) => {
+    setPage((draft) => {
+      draft.nodes[nodeIndex].type = type
+      draft.nodes[nodeIndex].value = ''
+    })
+  }
+
+  const setNodes = (nodes: NodeData[]) => {
+    setPage((draft) => (draft.nodes = nodes))
+  }
+
+  const setTitle = (title: string) => {
+    setPage((draft) => (draft.title = title))
+  }
+
+  const setCover = (cover: string) => {
+    setPage((draft) => (draft.cover = cover))
+  }
+
+  return {
+    nodes: page.nodes,
+    title: page.title,
+    cover: page.cover,
+    addNode,
+    removeNodeByIndex,
+    changeNodeValue,
+    changeNodeType,
+    setNodes,
+    setTitle,
+    setCover,
+  }
+}
