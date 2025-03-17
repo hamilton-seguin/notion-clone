1d6626285717fef8c821a8e02bfee75101b9053b
Hamilton <>
2025-03-17 11:53:03 +0100

    Wrap App with AppStateProvider and initialise with createPage util

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Page } from '@/page'
 import ThemeToggle from '@/components/ThemeToggle'
 
+import { AppStateProvider } from '@/context'
+import { createPage } from '@/utils'
+
+const initialState = createPage()
+
 function App() {
   return (
-    <main className="flex flex-col p-4 h-dvh bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text">
-      <ThemeToggle />
-      <div className="flex flex-col gap-4 justify-start items-center">
-       <Page />
-      </div>
-    </main>
+    <AppStateProvider initialState={initialState}>
+      <main className="flex flex-col p-4 h-dvh bg-light-bg dark:bg-dark-bg text-light-text dark:text-dark-text">
+        <ThemeToggle />
+        <div className="flex flex-col gap-4 justify-start items-center">
+          <Page />
+        </div>
+      </main>
+    </AppStateProvider>
   )
 }
 
