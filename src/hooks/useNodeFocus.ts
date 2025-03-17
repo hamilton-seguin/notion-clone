3f3dda1f7a24045c87e8f9f910396eced4f9d9fb
Hamilton <>
2025-03-17 17:59:52 +0100

    Create useNodeFocus to handle current node to focus

---
+++ src/hooks/useNodeFocus.ts
@@ -0,0 +1,21 @@
+import { useEffect, RefObject } from 'react'
+
+export const useNodeFocus = (
+  isFocused: boolean,
+  nodeRef: RefObject<HTMLDivElement | null>,
+  nodeValue: string
+) => {
+  useEffect(() => {
+    if (isFocused) {
+      nodeRef.current?.focus()
+    } else {
+      nodeRef.current?.blur()
+    }
+  }, [isFocused, nodeRef])
+
+  useEffect(() => {
+    if (nodeRef.current && !isFocused) {
+      nodeRef.current.textContent = nodeValue
+    }
+  }, [nodeValue, isFocused, nodeRef])
+}
