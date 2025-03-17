efbc7b7cd9ab8059e75894f708c0c1c9974aaefb
Hamilton <>
2025-03-17 17:43:18 +0100

    Create CommandPanel to allow selection of different NodeTypes

---
+++ src/components/CommandPanel.tsx
@@ -0,0 +1,41 @@
+import { NodeType } from '@/types'
+import { useOverflowsScreenBottom } from '@/hooks'
+
+import { SUPPORTED_NODE_TYPE } from '@/constants'
+
+export const CommandPanel = ({
+  selectedItemIndex,
+  selectItem,
+}: {
+  selectedItemIndex: number
+  selectItem: (nodeType: NodeType) => void
+}) => {
+  const { overflows, ref } = useOverflowsScreenBottom()
+
+  return (
+    <div
+      ref={ref}
+      className={`absolute z-10 -bottom-2.5 left-16 bg-light-bg w-48 p-1.5 transform rounded shadow-lg dark:bg-dark-bg ${
+        overflows ? '-translate-y-12' : 'translate-y-full'
+      }`}
+    >
+      <div className="font-bold p-3">Blocks</div>
+      <ul className="p-0 m-0 w-full list-none">
+        {SUPPORTED_NODE_TYPE.map((type, index) => {
+          const selected = selectedItemIndex === index
+          return (
+            <li
+              key={type.value}
+              onClick={() => selectItem(type.value)}
+              className={`p-3 cursor-pointer rounded hover:bg-orange-200 dark:hover:bg-slate-800 ${
+                selected ? 'bg-orange-300 dark:bg-slate-700' : ''
+              }`}
+            >
+              {type.name}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
