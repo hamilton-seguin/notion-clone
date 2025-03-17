956a0fa6280a3abef0494cdd9cd2ef64df0bccb7
Hamilton <>
2025-03-17 18:02:06 +0100

    Create getNodeStyle helper to style new nodes depending on their type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { nanoid } from 'nanoid'
 
-import { Page } from '@/types'
+import { NodeType, Page } from '@/types'
 
 export const createPage = () => {
   const slug = nanoid()
@@ -13,4 +13,23 @@ export const createPage = () => {
     cover: './notion-clone-cover.png',
   }
   return page
+}
+
+export const getNodeStyle = (
+  type: Omit<NodeType, 'page' | 'image'>
+): string => {
+  switch (type) {
+    case 'text':
+      return ''
+    case 'list':
+      return 'list-item list-disc'
+    case 'heading1':
+      return 'text-4xl font-bold'
+    case 'heading2':
+      return 'text-3xl font-bold'
+    case 'heading3':
+      return 'text-2xl font-bold'
+    default:
+      return ''
+  }
 }
