1577d88d897e07b9dd29896a4cc043c05bc25ea6
Hamilton <>
2025-03-17 15:25:10 +0100

    Update ThemeToggle to recognise system theme pref

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,17 +4,23 @@ export const ThemeToggle = () => {
   const [isDark, setIsDark] = useState(false)
 
   useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    setIsDark(mediaQuery.matches)
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsDark(e.matches)
     }
+    mediaQuery.addEventListener('change', handleChange)
+    return () => mediaQuery.removeEventListener('change', handleChange)
+  }, [])
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark')
   }, [isDark])
 
   const handleToggle = (e: ChangeEvent<HTMLInputElement>) => {
     setIsDark(e.target.checked)
-    document.body.classList.toggle('dark')
   }
+
   return (
     <div className="flex justify-end items-center">
       <label className="inline-flex items-center cursor-pointer relative">
@@ -24,8 +30,8 @@ export const ThemeToggle = () => {
           checked={isDark}
           className="sr-only peer"
         />
-        <div className="w-14 h-8 bg-gray-200 rounded-full peer dark:bg-zinc-600 hover:bg-gray-200/80 hover:dark:bg-zinc-600/80 peer-hover:dark:bg-zinc-600/80 peer-hover:bg-gray-200/80"></div>
-        <div className="absolute top-1 left-1 h-6 w-6 rounded-full bg-white flex items-center justify-center transform transition-transform duration-300 peer-checked:translate-x-6 peer">
+        <div className="w-14 h-8 rounded-full peer bg-light-border/70 hover:bg-light-border peer-hover:bg-light-border dark:bg-dark-border/80 hover:dark:bg-dark-border peer-hover:dark:bg-dark-border" />
+        <div className="absolute top-1 left-1 h-6 w-6 rounded-full bg-light-bg dark:bg-light-border flex items-center justify-center transform transition-transform duration-300 peer-checked:translate-x-6 peer">
           {isDark ? (
             <img src="./moon.svg" alt="Dark mode" className="h-4.5 w-4.5" />
           ) : (
