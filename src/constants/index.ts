8c675122c204c340de46281113dc0bc62e39ad73
Hamilton <>
2025-03-17 17:44:29 +0100

    export const SUPPORTED_NODE_TYPE & TEXT_NODE_TYPES

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+import { SupportedNodeType, NodeType } from "@/types";
+
+export const SUPPORTED_NODE_TYPE: SupportedNodeType[] = [
+  { value: 'text', name: 'Text' },
+  { value: 'list', name: 'List' },
+  { value: 'heading1', name: 'Heading 1' },
+  { value: 'heading2', name: 'Heading 2' },
+  { value: 'heading3', name: 'Heading 3' },
+]
+
+export const TEXT_NODE_TYPES: NodeType[] = [
+  'text',
+  'list',
+  'heading1',
+  'heading2',
+  'heading3',
+]
