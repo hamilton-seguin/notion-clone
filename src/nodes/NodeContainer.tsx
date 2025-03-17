d237efd4f83cf858db36e4423f0e7d702450b144
Hamilton <>
2025-03-17 18:13:56 +0100

    Create NodeContainer to be able to drag and reorder nodes

---
+++ src/nodes/NodeContainer.tsx
@@ -0,0 +1,43 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+
+import { NodeTypeSwitcher } from '@/nodes'
+
+import { NodeProps } from '@/types'
+
+export const NodeContainer = ({
+  node,
+  index,
+  updateFocusedIndex,
+  isFocused,
+}: NodeProps) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: node.id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="flex relative pl-4"
+      {...attributes}
+    >
+      <div
+        className="p-1.5 cursor-grab font-bold opacity-0 transition-opacity hover:opacity-100"
+        {...listeners}
+      >
+        ⠿
+      </div>
+      <NodeTypeSwitcher
+        node={node}
+        index={index}
+        updateFocusedIndex={updateFocusedIndex}
+        isFocused={isFocused}
+      />
+    </div>
+  )
+}
