93ab5c320e1ace7bf2b707d7ea51988811ca8d05
Hamilton <>
2025-03-17 19:58:08 +0100

    Add dnd to Page nodes with new PageContainer

--- src/page/Page.tsx
+++ src/page/Page.tsx
@@ -1,28 +1,41 @@
 import { nanoid } from 'nanoid'
+import { DndContext, DragEndEvent, DragOverlay } from '@dnd-kit/core'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 import { Cover, Spacer, Title } from '@/page'
-import { NodeTypeSwitcher } from '@/nodes'
+import { NodeContainer } from '@/nodes'
 
 import { useFocusedNodeIndex, useAppState } from '@/hooks'
 
 export const Page = () => {
-  const { title, nodes, setTitle, addNode } = useAppState()
+  const { title, nodes, setTitle, addNode, reorderNodes } = useAppState()
   const [focusedNodeIndex, setFocusedNodeIndex] = useFocusedNodeIndex({ nodes })
 
+  const handleDragEvent = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (over?.id && active.id !== over.id) {
+      reorderNodes(active.id as string, over.id as string)
+    }
+  }
   return (
     <>
       <Cover />
       <div className="flex flex-col items-center justify-start w-full gap-1 pl-12">
         <Title title={title} changePageTitle={setTitle} addNode={addNode} />
-        {nodes.map((node, index) => (
-          <NodeTypeSwitcher
-            key={node.id}
-            node={node}
-            updateFocusedIndex={setFocusedNodeIndex}
-            isFocused={focusedNodeIndex === index}
-            index={index}
-          />
-        ))}
+        <DndContext onDragEnd={handleDragEvent}>
+          <SortableContext items={nodes} strategy={verticalListSortingStrategy}>
+            {nodes.map((node, index) => (
+              <NodeContainer
+                key={node.id}
+                node={node}
+                updateFocusedIndex={setFocusedNodeIndex}
+                isFocused={focusedNodeIndex === index}
+                index={index}
+              />
+            ))}
+          </SortableContext>
+          <DragOverlay />
+        </DndContext>
         <Spacer
           showHint={!nodes.length}
           handleClick={() => {
