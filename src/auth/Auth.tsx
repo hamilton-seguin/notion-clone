6bc4ddb46b2d21182174e6c4e563ebb304ff2675
Hamilton <>
2025-03-18 13:44:13 +0100

    Update css

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -13,10 +13,12 @@ export const Auth = () => {
 
   return (
     <div className="w-full h-dvh flex justify-center items-center">
-      <div className="w-96 p-4 bg-white rounded-lg shadow-lg">
+      <div className="w-96 px-4 py-6 bg-white dark:bg-zinc-700 rounded-lg shadow-lg flex flex-col gap-6">
         <h1 className="text-2xl font-bold text-center">Notion Clone App</h1>
-        {loading ? (<div className='flex justify-center items-center px-4 py-8'>Sending magic link...</div>
-          
+        {loading ? (
+          <div className="flex justify-center items-center">
+            Sending magic link...
+          </div>
         ) : (
           <form>
             <label htmlFor="email">Email: </label>
@@ -26,16 +28,16 @@ export const Auth = () => {
               placeholder="Your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 mt-2 border border-light-border dark:border-dark-border rounded-lg"
+              className="w-full p-2 -mt-2 border border-light-border rounded-lg placeholder:text-light-border"
             />
             <button
               type="submit"
               onClick={() => {
                 setLoading(true)
               }}
-              className="w-full p-2 mt-2 bg-violet-500 text-white rounded-lg cursor-pointer"
+              className="w-full p-2 bg-orange-100 hover:bg-orange-200/60 text-light-text dark:text-dark-text dark:bg-gray-900 hover:dark:bg-gray-900/70 rounded-lg cursor-pointer"
             >
-              Submit
+              Send magic link
             </button>
           </form>
         )}
