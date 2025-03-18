7ebd2f4aa7f9ab331e67b3ce92f8bb73886bfd89
Hamilton <>
2025-03-18 13:10:45 +0100

    Wrap App in ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from 'react-router'
 import { AuthSessionProvider } from '@/context/provider'
 
 import App from './App.tsx'
+import { ThemeProvider } from './context/provider/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthSessionProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </AuthSessionProvider>
     </BrowserRouter>
   </StrictMode>
