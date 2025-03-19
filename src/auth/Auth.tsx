3eea17d3efbc0a9ebb4724a43c41faf9d9fd70fa
Hamilton <>
2025-03-19 11:15:45 +0100

    Fix auth form error + display Loader

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { Navigate } from 'react-router'
 
+import { Loader } from '@/components'
+
+import { supabase } from '@/supabaseClient'
 import { useAuthSession } from '@/hooks'
 
 export const Auth = () => {
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
 
-  const { session } = useAuthSession()
+  const { session, loading: authLoading } = useAuthSession()
 
-  if (session) <Navigate to="/" />
+  if (authLoading) return <Loader />
+
+  if (session) return <Navigate to="/" />
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      setLoading(true)
+      const { data, error } = await supabase.auth.signInWithOtp({
+        email,
+        options: {
+          emailRedirectTo: 'http://localhost:5173/',
+        }
+      })
+      if (error) throw new Error(`Failed to send magic link: ${error}`)
+      if (data) alert('Magic link sent: check your emails')
+    } catch (error) {
+      console.error('Error sending magic link: ', error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <div className="w-full h-dvh flex justify-center items-center">
@@ -20,7 +44,7 @@ export const Auth = () => {
             Sending magic link...
           </div>
         ) : (
-          <form>
+          <form onSubmit={handleLogin} className="flex flex-col gap-4">
             <label htmlFor="email">Email: </label>
             <input
               type="email"
@@ -32,9 +56,6 @@ export const Auth = () => {
             />
             <button
               type="submit"
-              onClick={() => {
-                setLoading(true)
-              }}
               className="w-full p-2 bg-orange-100 hover:bg-orange-200/60 text-light-text dark:text-dark-text dark:bg-gray-900 hover:dark:bg-gray-900/70 rounded-lg cursor-pointer"
             >
               Send magic link
