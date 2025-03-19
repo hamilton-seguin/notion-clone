0bf57fd04d7d9adc0a524a6fd31e1fcaf0aca53a
Hamilton <>
2025-03-19 10:33:05 +0100

    Create withInitialState HOC to wrap AppStateProvider with state from db depending on the slug

---
+++ src/state/withInitialState.tsx
@@ -0,0 +1,84 @@
+import { useMatch } from 'react-router'
+import { useState, useEffect, ComponentType } from 'react'
+
+import startPageScaffold from '@/state/startPageScaffold.json'
+import { Loader } from '@/components'
+
+import { supabase } from '@/supabaseClient'
+import { Page } from '@/types'
+
+type InjectedProps = {
+  initialState: Page
+}
+
+type PropsWithoutInjected<TBaseProps> = Omit<TBaseProps, keyof InjectedProps>
+
+export function withInitialState<TProps>(
+  WrappedComponent: ComponentType<PropsWithoutInjected<TProps> & InjectedProps>
+) {
+  return (props: PropsWithoutInjected<TProps>) => {
+    const match = useMatch('/:slug')
+    const pageSlug = match ? match.params.slug : 'start'
+
+    const [initialState, setInitialState] = useState<Page | null>()
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState<Error | undefined>()
+
+    useEffect(() => {
+      setIsLoading(true)
+      const fetchInitialState = async () => {
+        try {
+          const { data: userData } = await supabase.auth.getUser()
+          const user = userData.user
+          if (!user) {
+            throw new Error('User is not logged in')
+          }
+          const { data, error } = await supabase
+            .from('pages')
+            .select('title, id, cover, nodes, slug')
+            .match({ slug: pageSlug, created_by: user.id })
+            .single()
+          console.log('data of select: ', data)
+          console.log('error of select: ', error)
+
+          if (!data && pageSlug === 'start') {
+            const result = await supabase
+              .from('pages')
+              .insert({
+                ...startPageScaffold,
+                slug: 'start',
+                created_by: user.id,
+              })
+              .select()
+              .single()
+            console.log('result of insert: ', result)
+
+            setInitialState(result?.data)
+          } else {
+            setInitialState(data)
+          }
+        } catch (e) {
+          if (e instanceof Error) {
+            setError(e)
+          }
+        }
+        setIsLoading(false)
+      }
+      fetchInitialState()
+    }, [pageSlug])
+
+    if (isLoading) return <Loader />
+
+    if (error) return <div>{error.message}</div>
+
+    if (!initialState) {
+      return (
+        <div className="w-full h-full flex justify-center items-center">
+          Page not found
+        </div>
+      )
+    }
+
+    return <WrappedComponent {...props} initialState={initialState} />
+  }
+}
