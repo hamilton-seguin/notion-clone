11cd22962a8734c2b66a502b0eb9f9c599555464
Hamilton <>
2025-03-19 12:22:59 +0100

    Create PageNode to handle creating new /page on click

---
+++ src/nodes/PageNode.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+
+import { NodeData } from '@/types'
+import { useAppState } from '@/hooks'
+import { supabase } from '@/supabaseClient'
+
+export const PageNode = ({
+  node,
+  isFocused,
+  index,
+}: {
+  node: NodeData
+  isFocused: boolean
+  index: number
+}) => {
+  const navigate = useNavigate()
+  const [pageTile, setPageTitle] = useState('')
+
+  const { removeNodeByIndex } = useAppState()
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      e.preventDefault()
+      if (e.key === 'Backspace') {
+        removeNodeByIndex(index)
+      }
+      if (e.key === 'Enter') {
+        navigate(`/${node.value}`)
+      }
+      if (isFocused) {
+        window.addEventListener('keydown', handleKeyDown)
+      } else {
+        window.removeEventListener('keydown', handleKeyDown)
+      }
+      return () => {
+        window.removeEventListener('keydown', handleKeyDown)
+      }
+    }
+  }, [isFocused, index, node.value, removeNodeByIndex, navigate])
+
+  useEffect(() => {
+    const fetchPageTitle = async () => {
+      const { data } = await supabase
+        .from('pages')
+        .select('title')
+        .eq('slug', node.value)
+        .single()
+      setPageTitle(data?.title)
+    }
+    if (node.type === 'page' && node.value) {
+      fetchPageTitle()
+    }
+  }, [node.type, node.value])
+
+  const navigateToPage = () => {
+    navigate(`/${node.value}`)
+  }
+  return (
+    <div onClick={navigateToPage} className="cursor-pointer font-bold">
+      📄 {pageTile}
+    </div>
+  )
+}
