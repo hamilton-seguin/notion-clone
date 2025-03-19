b04c1f967771a5b7d96300f67c5bfbdc9ec0ba59
Hamilton <>
2025-03-19 10:24:01 +0100

    Update constans to include Image and Page nodes in CommandPanel

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,10 @@
-import { SupportedNodeType, NodeType } from "@/types";
+import { SupportedNodeType, NodeType } from '@/types'
 
 export const SUPPORTED_NODE_TYPE: SupportedNodeType[] = [
   { value: 'text', name: 'Text' },
   { value: 'list', name: 'List' },
+  { value: 'page', name: 'Page' },
+  { value: 'image', name: 'Image' },
   { value: 'heading1', name: 'Heading 1' },
   { value: 'heading2', name: 'Heading 2' },
   { value: 'heading3', name: 'Heading 3' },
