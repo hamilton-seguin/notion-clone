cf365babca88a16cf566539afb1ee5dc929112b7
Hamilton <>
2025-03-19 15:01:28 +0100

    Fix warning preventing hotreload (inline calling of withInitialState)

--- src/context/provider/AppStateProvider.tsx
+++ src/context/provider/AppStateProvider.tsx
@@ -5,17 +5,31 @@ import { usePageState } from '@/hooks'
 import { AppStateContext } from '@/context'
 import { withInitialState } from '@/state/withInitialState'
 
-export const AppStateProvider = withInitialState<{
+export const AppStateProviderComponent = ({
+  children,
+  initialState,
+}: {
   children: ReactNode
   initialState: Page
-}>(
-  ({ children, initialState }: { children: ReactNode; initialState: Page }) => {
-    const pageStateHandlers = usePageState(initialState)
+}) => {
+  const pageStateHandlers = usePageState(initialState)
 
-    return (
-      <AppStateContext.Provider value={pageStateHandlers}>
-        {children}
-      </AppStateContext.Provider>
-    )
-  }
-)
+  return (
+    <AppStateContext.Provider value={pageStateHandlers}>
+      {children}
+    </AppStateContext.Provider>
+  )
+}
+
+// Fix to HotReload issue: move withInitialState to after AppStateProviderComponent is defined
+AppStateProviderComponent.displayName = 'AppStateProviderComponent'
+
+const WrappedAppStateProvider = withInitialState<{
+  children: React.ReactNode
+}>(AppStateProviderComponent) as React.FC<{
+  children: React.ReactNode
+}>
+
+WrappedAppStateProvider.displayName = 'AppStateProvider'
+
+export const AppStateProvider = WrappedAppStateProvider
