2a0b70aeaa3f9c09f21c0b103f2e1ded31e236df
Hamilton <>
2025-03-22 16:09:53 +0100

    Remove parent div to add it to PageLayout

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -11,26 +11,24 @@ export const ThemeToggle = () => {
   }
 
   return (
-    <div className="flex justify-end items-center pb-4">
-      <label className="inline-flex items-center cursor-pointer relative">
-        <input
-          type="checkbox"
-          onChange={handleToggle}
-          checked={isDark}
-          className="sr-only peer"
-        />
-        <div className="w-14 h-8 rounded-full peer bg-light-border/70 hover:bg-light-border peer-hover:bg-light-border dark:bg-dark-border/80 hover:dark:bg-dark-border peer-hover:dark:bg-dark-border" />
-        <div className="absolute top-1 left-1 h-6 w-6 rounded-full bg-light-bg dark:bg-light-border flex items-center justify-center transform transition-transform duration-300 peer-checked:translate-x-6 peer">
-          {isDark ? (
-            <img src="./moon.svg" alt="Dark mode" className="h-4.5 w-4.5" />
-          ) : (
-            <img src="./sun.svg" alt="Light mode" className="h-4.5 w-4.5" />
-          )}
-        </div>
-        <span className="ml-3 text-sm font-medium">
-          {isDark ? 'dark mode' : 'light mode'}
-        </span>
-      </label>
-    </div>
+    <label className="inline-flex items-center cursor-pointer relative">
+      <input
+        type="checkbox"
+        onChange={handleToggle}
+        checked={isDark}
+        className="sr-only peer"
+      />
+      <div className="w-14 h-8 rounded-full peer bg-light-border/70 hover:bg-light-border peer-hover:bg-light-border dark:bg-dark-border/80 hover:dark:bg-dark-border peer-hover:dark:bg-dark-border" />
+      <div className="absolute top-1 left-1 h-6 w-6 rounded-full bg-light-bg dark:bg-light-border flex items-center justify-center transform transition-transform duration-300 peer-checked:translate-x-6 peer">
+        {isDark ? (
+          <img src="./moon.svg" alt="Dark mode" className="h-4.5 w-4.5" />
+        ) : (
+          <img src="./sun.svg" alt="Light mode" className="h-4.5 w-4.5" />
+        )}
+      </div>
+      <span className="ml-3 text-sm font-medium">
+        {isDark ? 'dark mode' : 'light mode'}
+      </span>
+    </label>
   )
 }
