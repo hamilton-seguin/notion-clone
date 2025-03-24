339d307bf00eb0088af4fa5ddce1ec7bcd331f6b
Hamilton <>
2025-03-24 17:24:38 +0100

    Add PageListToggle to PageLayout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,9 +1,12 @@
-import { ThemeToggle, Page } from '@/components'
+import { ThemeToggle, Page, PagesListToggle } from '@/components'
 
 export const PageLayout = () => {
   return (
     <main className="flex flex-col p-4 h-vh">
-      <ThemeToggle />
+      <div className="flex justify-end items-center pb-4 gap-4">
+        <PagesListToggle />
+        <ThemeToggle />
+      </div>
       <div className="flex flex-col gap-4 justify-start items-center">
         <Page />
       </div>
